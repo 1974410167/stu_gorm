7a3a4569b945c7d20081a86ddde267e85eb73070
hyuan <1974410167@qq.com>
2022-05-17 23:04:04 +0800

    5.17xuexi

--- file1/search.go
+++ file1/search.go
@@ -13,6 +13,20 @@ func SearchStudent(){
 	// id 为 1 student
 	DB.Model(&Student{}).Where("id = ?", 1).Find(&student)
 	// id为1的student对应的老师
+	fmt.Println(student)
+	var students3 []Student
+	DB.Model(&Student{}).Preload("Teachers").Find(&students3)
+	fmt.Printf("%+v", students3)
 	DB.Model(student).Association("Teachers").Find(&teachers)
 	fmt.Println(teachers[0])
+
+	var school School
+	//var students []*Student
+	DB.Model(&School{}).Where("name = ?", "东路小学").First(&school)
+	//DB.Model(&Student{}).Where("school_id = ?", school.ID).Find(&students)
+	fmt.Println(school)
+
+	var students1 []Student
+	DB.Model(&school).Association("Students").Find(&students1)
+	fmt.Println(students1)
 }
